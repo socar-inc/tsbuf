8f0d87ec9ddc485047da6d898e4e2648b766c2c4
socar-dot <dot@socar.kr>
2019-06-19 21:11:04 +0900

    enum 모듈 수정

--- dist/typescript/generator/enum.js
+++ dist/typescript/generator/enum.js
@@ -4,6 +4,6 @@ var const_1 = require("./const");
 exports.generateEnum = function (mode) {
     if (mode === void 0) { mode = const_1.GenerateMode.Global; }
     return function (ast) {
-        return "\n    " + (mode === const_1.GenerateMode.Global ? 'declare' : 'export') + " enum " + ast.name.name + " {\n      " + ast.body.map(function (f) { return f.name.name + " = " + f.name.name + ","; }).join('\n') + "\n    }\n  ";
+        return "\n    " + (mode === const_1.GenerateMode.Global ? 'declare' : 'export') + " enum " + ast.name.name + " {\n      " + ast.body.map(function (f) { return f.name.name + " = '" + f.name.name + "',"; }).join('\n') + "\n    }\n  ";
     };
 };
